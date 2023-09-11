9a94b1825451fda76edf84f8e99d40420fce9f5d
yahir <pixelMPG123@gmail.com>
2023-09-10 19:45:50 -0600

    version 2.4

--- hola.java
+++ hola.java
@@ -1,10 +1,6 @@
 public class hola{
      public static void main(String[] args) {
         System.out.println("hola mundo");
-        System.out.println("Este es un segundo commit");
-        String a, b;
-        a= "String a ";
-        b= "String b";
     }
     
 }
